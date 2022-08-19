dc9df0dd6ae358537f1a30b889d7ba803ff82141
Your Name <pedro.fer.98@gmail.com>
2022-08-18 21:11:13 -0400

    image history works. removed the forEach loopig in the getImage fucntion as well

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const imageButton = document.querySelector("#imageButton");
 const quoteButton = document.querySelector("#quoteButton");
 let actualQuote = quote.innerHTML;
 let quoteHistory =[];
+let imagehistory =[];
 // variables to access dataArray
 
 
@@ -23,24 +24,7 @@ quoteButton.addEventListener("click",getQuote);
 
 
 //FUNCTIONS
-//still need to get all the quotes and not just the first page
-// function getQuote(){
-//     try {
-//       fetch("https://api.quotable.io/quotes")
-//       .then(res => res.json())
-//       .then(data => {
-//         const quotesArray = data.results;
-//         quotesArray.forEach(individualQuote => {
-//             let randomNumber = Math.floor(Math.random()*quotesArray.length)
-//             quote.innerHTML = `"${quotesArray[randomNumber].content}"`
-//             author.innerHTML = `"${quotesArray[randomNumber].author}"`
-//             });
-//         console.log(data)
-//       });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+
 function getQuote(){
   
   try {
@@ -48,10 +32,7 @@ function getQuote(){
     .then(res => res.json())
     .then(data => {
       const quotesArray = data.results;
-       // where i will push the quotes generated with my fucntion. this array will be the "history"
-      
-
-      
+       
           let randomNumber = Math.floor(Math.random()*quotesArray.length)
           let randomQuote = quotesArray[randomNumber].content;
           let randomAuthor = quotesArray[randomNumber].author
@@ -60,18 +41,7 @@ function getQuote(){
           let actualQuote = quote.innerHTML // one option for "saving" the output.
           // console.log(actualQuote);
           quoteHistory.push({author: randomAuthor, quote: randomQuote});
-          
-   
 
-          // console.log(returnArray);
-          
-         
-          
-
-        
-
-      
-      
     });
     console.log(quoteHistory);
     // let actualQuote = quote.innerHTML;
@@ -90,15 +60,23 @@ function getImage(){
       fetch("https://picsum.photos/v2/list")
       .then(res => res.json())
       .then(data => {
+
         console.log(data);
-        data.forEach(individualimage => {
+        // data.forEach(individualimage => {
+
             let randomNumber = Math.floor(Math.random()*data.length)
-            image.setAttribute("src",`${data[randomNumber].download_url}`)
+            let randomImage = data[randomNumber].download_url;
+            console.log(randomImage);
+            image.setAttribute("src",`${randomImage}`);
+            imagehistory.push(randomImage);
+
         })
-      })
+
+      // })
     } catch (error) {
       console.log(error);
   }
+  console.log(imagehistory);
 }
 function getRandom(){
   getQuote();
