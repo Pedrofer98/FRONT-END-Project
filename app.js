40c1fc9c4acddb0d30e9b12bb8ebb9045f3a3b7d
David <mr.hilleke@gmail.com>
2022-08-20 10:04:46 -0400

    first commit to my branch, no merge

--- app.js
+++ app.js
@@ -24,21 +24,29 @@ quoteButton.addEventListener("click",getQuote);
 //still need to get all the quotes and not just the first page
 function getQuote(){
     try {
-      fetch("https://api.quotable.io/quotes")
+      fetch("https://api.quotable.io/quotes?page=103")
       .then(res => res.json())
       .then(data => {
-        const quotesArray = data.results;
-        quotesArray.forEach(individualQuote => {
-            let randomNumber = Math.floor(Math.random()*quotesArray.length)
-            quote.innerHTML = `"${quotesArray[randomNumber].content}"`
-            author.innerHTML = `"${quotesArray[randomNumber].author}"`
-            });
-        console.log(data)
-      });
-    } catch (error) {
+        let newQuotesArray = []
+        for (i = 0; i <= data.totalPages; i++) {
+          newQuotesArray.push(...data.results)
+        }
+        return newQuotesArray
+      })
+      .then((newQuotesArray) => {
+          renderQuote(newQuotesArray)
+    })
+      } catch (error) {
         console.log(error);
     }
 }
+
+function renderQuote (array){
+    let randomNumber = Math.floor(Math.random()*array.length)
+    quote.innerHTML = `"${array[randomNumber].content}"`
+    author.innerHTML = `"${array[randomNumber].author}"`
+  }
+
 
 //still need to get all the images and not just the first page
 function getImage(){
@@ -46,16 +54,25 @@ function getImage(){
       fetch("https://picsum.photos/v2/list")
       .then(res => res.json())
       .then(data => {
-        console.log(data);
-        data.forEach(individualimage => {
-            let randomNumber = Math.floor(Math.random()*data.length)
-            image.setAttribute("src",`${data[randomNumber].download_url}`)
+        console.log(data)
+        // let imagesArray = []
+        // for (i = 0; i <= data.totalPages; i++) {
+        //   imagesArray.push(...data.results)
+        // }
+        // return newQuotesArray
+        //     let randomNumber = Math.floor(Math.random()*data.length)
+        //     image.setAttribute("src",`${data[randomNumber].download_url}`)
         })
-      })
-    } catch (error) {
+      } catch (error) {
       console.log(error);
   }
 }
+
+function renderImage(){
+  let randomNumber = Math.floor(Math.random()*data.length)
+  image.setAttribute("src",`${data[randomNumber].download_url}`)
+}
+
 function getRandom(){
   getQuote();
   getImage();
