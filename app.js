4740acf676b1f9a266e643deb565d5d7d9bca245
David <mr.hilleke@gmail.com>
2022-08-27 15:51:02 -0400

    first commit after big merge, history feature is working, taylor and kelly did lots of styling and dropdown stuff, still need to renderhistory from renderimage and getrandom functions

--- app.js
+++ app.js
@@ -4,62 +4,80 @@ const randomButton = document.querySelector("#randomButton");
 const author = document.querySelector(".author");
 const image = document.querySelector(".card-img");
 let dropdown = document.querySelector("select");
+let select = document.getElementById("categorySelection")
 let selectedCategory = null;
 const imageButton = document.querySelector("#imageButton");
 const quoteButton = document.querySelector("#quoteButton");
+const dropdownButton = document.querySelector("#dropdownButton");
+const imageInHistory = document.querySelector(".card-img-Hist1");
+const quoteInHistory = document.querySelector(".quote-text-history1");
+const authorInHistory = document.querySelectorAll(".author-History1");
+const historyDeckToAppend = document.getElementById("history-deck");
 
 
-let actualQuote = quote.innerHTML;
-let quoteHistory = [];
-let imagehistory = [];
-let categories = ["sports", "competition"];
 let newQuotesArray = [];
 let imagesArray = [];
+let tagArray = [];
 
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const randomPage = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 let counter = 0;
 
-
+// select.innerHTML ="";
 
 //EVENT LISTENERS
-window.addEventListener("load", getQuote);
-window.addEventListener("load", getImage);
+window.addEventListener("load", onLoad);
+//single event handler for load function, then call image, then call quote
+
+
+//window.addEventListener("load", fillDropdown)
 randomButton.addEventListener("click",getRandom);
 imageButton.addEventListener("click",renderImage);
 quoteButton.addEventListener('click', renderQuote)
-
-
-//FUNCTIONS
-if (dropdown)
-  dropdown.addEventListener("change", function (event) {
+dropdownButton.addEventListener("click", fillDropdown);
+dropdown.addEventListener("change", function (event) {
     selectedCategory = event.target.value;
     //fillDropdown();
     getQuoteFromCategory();
   });
 
+//FUNCTIONS
+function fillDropdown() {
+      if (counter ===99){
+      newQuotesArray = [];
+      imagesArray = [];
+      getQuote();
+      getImage();
+      counter = 0;
+    }
+    counter += 1;
+    //console.log(newQuotesArray.tags)
+    newQuotesArray.map(
+     (quote) => tagArray.push(...quote.tags)
+    )
+   let noDuplicates = tagArray.filter((c, index) => {
+    return tagArray.indexOf(c)
+    === index;   })
+    console.log(noDuplicates)
+    for(i=0; i<noDuplicates.length; i++) {
+      let option = document.createElement("option"),
+      text = document.createTextNode(noDuplicates[i]);
+      option.appendChild(text);
+      option.setAttribute("value", noDuplicates[i]);
+      select.insertBefore(option, select.lastChild);
 
-function getQuoteFromCategory() {
-  fetch("https://api.quotable.io/quotes")
-    .then((res) => res.json())
-    .then((data) => {
-      let newQuotesArray = data.results;
-      let categoryArray = newQuotesArray.filter(
-        (categoryArray) => categoryArray.tags[0] === selectedCategory
-      );
-      renderQuote(categoryArray);
-    });
-}
+    }
+  }
+   
 
 
-function getQuote(){
-  let quoteURL = `https://api.quotable.io/quotes?limit=100&page=${randomNumber(1, 14)}`
+
+async function getQuote(){
+  let quoteURL = `https://api.quotable.io/quotes?limit=100&page=${randomPage(1, 14)}`
   try {
-    fetch(quoteURL)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      newQuotesArray.push(...data.results)
-    })
+    let results = await fetch(quoteURL);
+    let data = await results.json();
+    newQuotesArray.push(...data.results)
+    //.then(renderQuote)
   }catch (error) {
       console.log(error);
   }
@@ -75,39 +93,72 @@ function renderQuote (){
       counter = 0;
     }
     counter += 1;
-    let actualQuote = quote.innerHTML;
     let randomNumber = Math.floor(Math.random()*newQuotesArray.length)
     let randomQuote = newQuotesArray[randomNumber].content;
     let randomAuthor = newQuotesArray[randomNumber].author;
     quote.innerHTML = `"${randomQuote}"`
     author.innerHTML = `~${randomAuthor}`
-    quoteHistory.push({author: randomAuthor, quote: randomQuote});
+    // renderHistory(image,randomQuote,randomAuthor)
+    
+    let historyItem = document.createElement('div');
+    historyItem.classList.add("col-4");
+    historyItem.innerHTML = 
+        `<div class="card bg-dark text-white">
+        <img src="${image.src}" class="card-img" alt="..."> 
+                  <div class="card-img-overlay">
+                      <p class="quote-text-history1">${randomQuote}</p>
+                      <p class="author-History1">-${randomAuthor}</p>
+                  </div>
+          </div>`;
+    historyDeckToAppend.appendChild(historyItem);
+    console.log(image.download_url);
+    }
 
-  }
 
-
-function getImage(){
+async function getImage(){
   try {
-      fetch(`https://picsum.photos/v2/list?limit=100`)
-      .then(res => res.json())
-      .then(data => {
-        //run recursion just like in getquote up to a static #
-        imagesArray.push(...data);
-    }) 
+      let data = await fetch(`https://picsum.photos/v2/list?limit=100`) ;
+      let data2 = await data.json();
+      // debugger;
+      imagesArray.push(...data2);
+      //.then(renderImage)
   }catch (error) {
       console.log(error);
-
   }
 }
 
 
 function renderImage(){
+  if (counter === 99){
+    newQuotesArray = [];
+    imagesArray = [];
+    getQuote();
+    getImage();
+    counter = 0;
+  }
+  counter += 1;
   let randomNumber = Math.floor(Math.random()*imagesArray.length)
   let randomImage = imagesArray[randomNumber].download_url;
   image.setAttribute("src",`${randomImage}`);
-  imagehistory.push(randomImage);
+  // renderHistory(randomImage)
 }
+  
 
+  // imageInHistory.setAttribute("src",`${randomImage}`);//alternative 1. issue:
+  // we need to din. create the HTML elements and have their src's set to the randomImg.
+
+// option 2
+  // historyDeckToAppend.appendChild(`
+  //      <div class="col-4">
+  //         <div class="card bg-dark text-white">
+  //             <img src="${randomImage}" class="card-img-Hist1" alt="...">
+  //                  <div class="card-img-overlay">
+  //                      <p class="quote-text-history1">${quote.innerHTML}</p>
+  //                      <p class="author-History1">-${author.innerHTML}</p>
+  //                 </div>
+  //          </div>
+  //      </div>`)
+ 
 
 function getRandom(){
   if (counter === 99){
@@ -120,5 +171,46 @@ function getRandom(){
   counter += 1;
   renderImage();
   renderQuote();
+}
 
+
+// function renderHistory (randomImage,){
+//   historyDeckToAppend.appendChild(`
+//        <div class="col-4">
+//           <div class="card bg-dark text-white">
+//               <img src="${randomImage}" class="card-img-Hist1" alt="...">
+//                   <div class="card-img-overlay">
+//                       <p class="quote-text-history1">${randomQuote}</p>
+//                       <p class="author-History1">-${randomAuthor}</p>
+//                   </div>
+//           </div>
+//       </div>`)
+// }
+
+function renderHistory(param1,param2,param3){
+  let historyItem = document.createElement('div');
+  historyItem.classList.add("col-4");
+  historyItem.innerHTML = 
+      `<div class="card bg-dark text-white">
+            <img src="${param1}" class="card-img-Hist1" alt="...">
+                <div class="card-img-overlay">
+                    <p class="quote-text-history1">${param2}</p>
+                    <p class="author-History1">-${param3}</p>
+                </div>
+        </div>`;
+  historyDeckToAppend.appendChild(historyItem);
+}
+
+function onLoad(){
+  Promise.all([getImage(), getQuote()])
+    .then(() => {
+      renderImage();
+      renderQuote();
+    });
+}
+
+
+
+function loadData(){
+  
 }
--- mockCode.js
+++ mockCode.js
@@ -1,192 +1,133 @@
-//POSSIBLE FAVORITES SECTION INSTEAD OF OR IN ADDITION TO HISTORY SECTION
-// const favoritesButton = document.querySelector('.favorites')
-// favoritesButton.addEventListener('click', FavList);
-
-// function FavList(event){
-//     if (checkbox.status == checked){
-//       //move gif to favorites list
-//     } else {
-//       //remove from favorites list
-//     }
-//   }
-
-//kelly changes
-// function fillDropdown() {
-//   fetch("https://api.quotable.io/quotes")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       let newQuotesArray = data.results;
-//       for (i = 0; i < newQuotesArray.length; i++) {
-//         let categories = newQuotesArray[i].tags[i];
-//         let next = document.createElement("option");
-//         next.textContent = categories;
-//         next.value = categories;
-//         selectedCategory.appendChild(next);
-//       }
-
-//let categoryArray = newQuotesArray[i].tags;
-//     });
-// }
-
-//WORKING GETQUOTE FUNCTION, PRIOR TO FIXING PAGINATION
-//   function getQuote(){
-//     try {
-//       fetch("https://api.quotable.io/quotes")
-//       .then(res => res.json())
-//       .then(data => {
-//         const quotesArray = data.results;
-//         let returnArray =[] // where i will push the quotes generated with my fucntion. this array will be the "history"
-//         const returnVar = quotesArray[randomNumber].content
-//         quotesArray.forEach(individualQuote => {
-//             let randomNumber = Math.floor(Math.random()*quotesArray.length)
-//             quote.innerHTML = `"${quotesArray[randomNumber].content}"`
-//             let actualQuote = quote.innerHTML // one option for "saving" the output.
-//             console.log('actualQuote');
-//             returnArray.push('actualQuote');
-//             author.innerHTML = `"${quotesArray[randomNumber].author}"`
-//             });
-//         console.log(data)
-//         return returnVar;
-//       });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-//WORKING GETQUOTE FUNCTION AFTER FIXING PAGINATION
-// function getQuote(){
-//     try {
-//       fetch("https://api.quotable.io/quotes")
-//       .then(res => res.json())
-//       .then(data => {
-//         let newQuotesArray = []
-//         for (i = 0; i <= data.totalPages; i++) {
-//           newQuotesArray.push(...data.results)
-//         }
-//         return newQuotesArray
-//       })
-//       .then((newQuotesArray) => {
-//         renderQuote(newQuotesArray);
-//         console.log(quoteHistory);
-//       })
-//       } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-//HISTORY FEATURE
-
-function addImageToHistory(){
-  history.createElement('img');
-  history.img.setAttribute('src',imagehistory.length);
-};
-
-function addQuoteToHistory(){
-  history.createElement('p');
-  history.p.setAttribute('class','quote-text-inHistory');
-  history.p.innerHTML = quoteHistory.quote+author; // would this substitute lines 112-114?
-  // same thing but adding the author to the quote//
-  history.createElement('p');
-  let pOfAuthor = history.p.setAttribute('class','author-text-inHistory');
-  history.pOfAuthor.innerHTML = quoteHistory.author; 
+//need Taylor's help to fix the CSS
+//hard code history div w/ template card
+//refactor code to appendChild to history div as well
+//connect drop down dynamically to current quote array's tags
+//merge pedro's code
 
 
-  //function history(quoteGenerated, authorGenerated,imageGenerated){
-// }
 
-// const getQuoteOutput = $(getQuote);
-// or add eventListener so that when the buttons are clicked, the output of the 
-// function is attributed to a variable
 
-quoteButton.addEventListener("click",function(){
-    getQuote() = getQuoteOutput;
-    console.log(getQuoteOutput);
-})
-quoteButton.addEventListener("click",function(){
-    const getQuoteOutput= getQuote();
-     console.log(getQuoteOutput);
-   })
-   
+//VARIABLES
+const quote = document.querySelector(".quote-text");
+const randomButton = document.querySelector("#randomButton");
+const author = document.querySelector(".author");
+const image = document.querySelector(".card-img");
+let dropdown = document.querySelector("select");
+let selectedCategory = null;
+const imageButton = document.querySelector("#imageButton");
+const quoteButton = document.querySelector("#quoteButton");
 
-//
 
-function getRandom(){
-    getQuote();
-    getImage();
+let actualQuote = quote.innerHTML;
+let quoteHistory = [];
+let imagehistory = [];
+let categories = ["sports", "competition"];
+let newQuotesArray = [];
+let imagesArray = [];
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+let counter = 0;
+
+
+
+//EVENT LISTENERS
+window.addEventListener("load", getQuote);
+window.addEventListener("load", getImage);
+randomButton.addEventListener("click",getRandom);
+imageButton.addEventListener("click",renderImage);
+quoteButton.addEventListener('click', renderQuote)
+
+
+//FUNCTIONS
+if (dropdown)
+  dropdown.addEventListener("change", function (event) {
+    selectedCategory = event.target.value;
+    //fillDropdown();
+    getQuoteFromCategory();
+  });
+
+
+function getQuoteFromCategory() {
+  fetch("https://api.quotable.io/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let newQuotesArray = data.results;
+      let categoryArray = newQuotesArray.filter(
+        (categoryArray) => categoryArray.tags[0] === selectedCategory
+      );
+      renderQuote(categoryArray);
+    });
 }
 
-
-
-{/* <div class='favorites'>
-      <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-            <img src="bao-bun.jpg" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <p class="quote-text">Let's get poppin.</p>
-                    <p class="author">-Jawann Brady</p>
-                </div>
-        </div>
-      </div> 
-    </div> */}
-
-
-
-const quoteURL = "https://api.quotable.io/quotes?limit=150"
-
-// HISTORY FEATURE:
-function addImageToHistory(){
-  history.createElement('img');
-  history.img.setAttribute('src',imagehistory.length);
-};
-
-function addQuoteToHistory(){
-  history.createElement('p');
-  history.p.setAttribute('class','quote-text-inHistory');
-  history.p.innerHTML = quoteHistory.quote+author; // would this substitute lines 112-114?
-  // same thing but adding the author to the quote//
-  history.createElement('p');
-  let pOfAuthor = history.p.setAttribute('class','author-text-inHistory');
-  history.pOfAuthor.innerHTML = quoteHistory.author;
-
-// //optimize getQuote and getImage functions, possibly split into two functions,
-//one to fetch/render the data into an array and one to get it into the function
-// function getData(){
-//     try {
-//       fetch("https://api.quotable.io/quotes")
-//       .then(res => res.json())
-//       .then(data => {
-//         return data;
-//       });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// function renderQuote(){
-
-// }
-
-    
-
-  if(data.totalPages > page){
-    await getQuote(page+1)
-  }
-}
 
 function getQuote(){
+  let quoteURL = `https://api.quotable.io/quotes?limit=100&page=${randomNumber(1, 14)}`
   try {
-      fetch(quoteURL)
-      .then(res => res.json())
-      .then(data => {
-        newQuotesArray.push(...data.results)
-        for (i=data.page; i < data.totalPages; i++) {  
-        getQuote() 
-        }
-      })
+    fetch(quoteURL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      newQuotesArray.push(...data.results)
+    })
   }catch (error) {
       console.log(error);
   }
 }
 
 
+function renderQuote (){
+    if (counter === 99){
+      newQuotesArray = [];
+      imagesArray = [];
+      getQuote();
+      getImage();
+      counter = 0;
+    }
+    counter += 1;
+    let actualQuote = quote.innerHTML;
+    let randomNumber = Math.floor(Math.random()*newQuotesArray.length)
+    let randomQuote = newQuotesArray[randomNumber].content;
+    let randomAuthor = newQuotesArray[randomNumber].author;
+    quote.innerHTML = `"${randomQuote}"`
+    author.innerHTML = `~${randomAuthor}`
+    quoteHistory.push({author: randomAuthor, quote: randomQuote});
 
+  }
+
+
+function getImage(){
+  try {
+      fetch(`https://picsum.photos/v2/list?limit=100`)
+      .then(res => res.json())
+      .then(data => {
+        //run recursion just like in getquote up to a static #
+        imagesArray.push(...data);
+    }) 
+  }catch (error) {
+      console.log(error);
+
+  }
+}
+
+
+function renderImage(){
+  let randomNumber = Math.floor(Math.random()*imagesArray.length)
+  let randomImage = imagesArray[randomNumber].download_url;
+  image.setAttribute("src",`${randomImage}`);
+  imagehistory.push(randomImage);
+}
+
+
+function getRandom(){
+  if (counter === 99){
+    newQuotesArray = [];
+    imagesArray = [];
+    getQuote();
+    getImage();
+    counter = 0;
+  }
+  counter += 1;
+  renderImage();
+  renderQuote();
+
+}
